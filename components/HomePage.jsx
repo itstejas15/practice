5572dc5bec82972375384c86f4a0e266c04af601
Tejas Patil <tejas_patil2@persistent.com>
2023-04-25 16:14:58 +0530

    used &apos;

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -4,10 +4,10 @@ const HomePage = () => {
       <div className="w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600">
-            LET&lsquo;S BUILD SOMETHING TOGETHER
+            LET&#39;S BUILD SOMETHING TOGETHER
           </p>
           <h1 className="py-4 text-gray-700">
-            Hi, I&lsquo;m <span className="text-[#5651e5]"> Tejas </span>
+            Hi, I&apos;m <span className="text-[#5651e5]"> Tejas </span>
           </h1>
           <h1 className="py-2 text-gray-700">A Front-End Web Developer </h1>
         </div>
