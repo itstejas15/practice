3e9a661149e63bb5ffed0c2e261948e045b1a4be
Tejas Patil <tejas_patil2@persistent.com>
2023-04-25 16:36:22 +0530

    adjusted

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -102,6 +102,7 @@ const Navbar = () => {
               ? " fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
               : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
           }
+          onClick={''}
         >
           <div>
             <div className="flex w-full items-center justify-between">
@@ -157,7 +158,7 @@ const Navbar = () => {
                 </li>
               </Link>
             </ul>
-            <div className="pt-26">
+            <div className="pt-27">
               <p className="uppercase tracking-widest text-[#5651e5]">
                 Let&#39;s Connect
               </p>
