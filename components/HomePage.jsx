3cbf6cb8b74bf61f3df0b7a875a698d5dc17e80b
Tejas Patil <tejas_patil2@persistent.com>
2023-04-25 16:05:04 +0530

    Created Navbar

---
+++ components/HomePage.jsx
@@ -0,0 +1,19 @@
+const HomePage = () => {
+  return (
+    <div className='w-full h-screen text-center'>
+      <div className="w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            LET&#39;S BUILD SOMETHING TOGETHER
+          </p>
+          <h1 className="py-4 text-gray-700">
+            Hi, I'm <span className="text-[#5651e5]"> Tejas </span>
+          </h1>
+          <h1 className="py-2 text-gray-700">A Front-End Web Developer </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ components/Navbar.jsx
@@ -0,0 +1,207 @@
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import TP from "../public/assets/icons/TP.png";
+import TPR from "../public/assets/icons/TP-Round.png";
+import Close from "../public/assets/icons/Close.png";
+import Hamburger from "../public/assets/icons/Hamburger.png";
+import Image from "next/image";
+import {FaLinkedinIn,FaGithub} from 'react-icons/fa';
+import {AiOutlineMail} from 'react-icons/ai';
+import {BsFillPersonLinesFill} from 'react-icons/bs';
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const [shadow, setShadow] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 65) {
+        setShadow(true);
+      } else {
+        setShadow(false);
+      }
+    };
+    window.addEventListener("scroll", handleShadow);
+  }, []);
+
+  return (
+    <div
+      style={{ backgroundColor: "#ecf0f3" }}
+      className={
+        shadow
+          ? "fixed w-full h-20 shadow-xl z-[100] ease-in-out duration-300"
+          : "fixed w-full h-20 z-[100]"
+      }
+    >
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Link href="/">
+          {/* <a> */}
+          <Image
+            src={TP}
+            alt="/"
+            width="115"
+            height="45"
+            className="cursor-pointer pt-6 fill-orange-700 "
+          />
+          {/* </a>  */}
+        </Link>
+        <div>
+          <ul style={{ color: "#1f2937" }} className="hidden md:flex pr-8">
+            <li className="ml-10 text-sm uppercase hover:border-b">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="ml-10 text-sm uppercase hover:border-b">
+              <Link href="/#about">About</Link>
+            </li>
+            <li className="ml-10 text-sm uppercase hover:border-b">
+              <Link href="/#skills">Skills</Link>
+            </li>
+            <li className="ml-10 text-sm uppercase hover:border-b">
+              <Link href="/#projects">Projects</Link>
+            </li>
+            <li className="ml-10 text-sm uppercase hover:border-b">
+              <Link href="/#resume">Resume</Link>
+            </li>
+            <li className="ml-10 text-sm uppercase hover:border-b">
+              <Link href="/#contact">Contact</Link>
+            </li>
+          </ul>
+          {/* Hamburger Icon */}
+          <div
+            style={{ color: "#1f2937" }}
+            onClick={handleNav}
+            className="md:hidden"
+          >
+            <Image
+              src={Hamburger}
+              alt="/"
+              width="25"
+              height="12"
+              className="cursor-pointer pt-4"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {/* Overlay */}
+      <div
+        className={
+          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""
+        }
+      >
+        {/* Side Drawer Menu */}
+        <div
+          className={
+            nav
+              ? " fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+          }
+        >
+          <div>
+            <div className="flex w-full items-center justify-between">
+              <Link href="/">
+                {/* <a> */}
+                <Image src={TPR} width="70" height="30" alt="/" />
+                {/* </a> */}
+              </Link>
+              <div
+                onClick={handleNav}
+                className="cursor-pointer"
+              >
+                <Image src={Close} width="30" height="10" alt="/" />
+              
+              </div>
+            </div>
+            <div className="border-b border-gray-300 my-4">
+              <p className="w-[85%] md:w-[90%] py-4">
+                Let&#39;s build something legendary together
+              </p>
+            </div>
+          </div>
+          <div className="py-4 flex flex-col">
+            <ul className="uppercase">
+              <Link href="/">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Home
+                </li>
+              </Link>
+              <Link href="/#about">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  About
+                </li>
+              </Link>
+              <Link href="/#skills">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Skills
+                </li>
+              </Link>
+              <Link href="/#projects">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Projects
+                </li>
+              </Link>
+              <Link href="/#resume">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Resume
+                </li>
+              </Link>
+              <Link href="/#contact">
+                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                  Contact
+                </li>
+              </Link>
+            </ul>
+            <div className="pt-40">
+              <p className="uppercase tracking-widest text-[#5651e5]">
+                Let&#39;s Connect
+              </p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <a
+                  href="https://www.linkedin.com/in/clint-briley-50056920a/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <FaLinkedinIn /> 
+                  </div>
+                </a>
+                <a
+                  href="https://github.com/fireclint"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                    <FaGithub /> 
+                  </div>
+                </a>
+                <Link href="/#contact">
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300"
+                  >
+                    <AiOutlineMail /> 
+                  </div>
+                </Link>
+                <Link href="/resume">
+                  <div
+                    onClick={() => setNav(!nav)}
+                    className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300"
+                  >
+                    <BsFillPersonLinesFill /> 
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
