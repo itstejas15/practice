e97c3ffff6fe7e7ad7647134e696778d542b7aec
Tejas Patil <tejas_patil2@persistent.com>
2023-04-25 17:16:37 +0530

    scroll

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -99,7 +99,7 @@ const Navbar = () => {
         <div
           className={
             nav
-              ? " fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+              ? " fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen scroll-auto bg-[#ecf0f3] p-10 ease-in duration-500"
               : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
           }
           onClick={''}
@@ -125,7 +125,7 @@ const Navbar = () => {
               </p>
             </div>
           </div>
-          <div className="py-4 flex flex-col snap-y">
+          <div className="py-4 flex flex-col">
             <ul className="uppercase">
               <Link href="/">
                 <li onClick={() => setNav(false)} className="py-4 text-sm">
