7180ced822f1c93dab82f05e13b2fb8e59dc815a
Tejas Patil <tejas_patil2@persistent.com>
2023-04-26 21:28:10 +0530

    lil change

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -14,10 +14,7 @@ const Skills = () => {
   return (
     <div id="skills" className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
-        <p className="text-xl mb-4 tracking-widest text-[#5651e5]">
-          {" "}
-          MY SKILLS
-        </p>
+        <p className="text-xl mb-4 tracking-widest text-[#5651e5]">MY SKILLS</p>
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
           <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
